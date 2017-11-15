8abc9cd9b6dfb61789791c54a8537567edd43fc2
300204 <33694553+300204@users.noreply.github.com>
2017-11-15 10:11:49 -0500

    Add files via upload

---
+++ StoreProject.java
@@ -0,0 +1,36 @@
+/*
+   Store Project
+   @author Michael Imbesi
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+import java.awt.Color;
+
+public class StoreProject
+{
+   public static void main(String[] args)
+   {
+      Scanner keyboard = new Scanner(System.in);               // reads user input
+      double quantity1 = 0;                                    // quantity of item ordered                        
+      double subtotal1 = 0.0;                                  // subtotal of customer's purchase                      
+      double taxAmount1 = 0.0;                                 // total amount of tax                  
+      double totalCost1 = 0.0;                                 // total cost of purchase                     
+      final double TAX_RATE = 0.06999;                         // tax rate
+      //prices of photos and paintings
+      final double RANDOM_PORTRAIT_PRICE = 10.0;               // price per portrait of random person              
+      final double CUSTOM_PORTRAIT_PRICE = 15.0;               // price per custom portrait photo
+      final double CUSTOM_PORTRAIT_PAINTING = 50.0;            // price per custom portrait painting
+      final double LANDSCAPE_PIC_PRICE = 20.0;                 // price per landscape photo
+      final double LANDSCAPE_PAINTING_PRICE = 30.0;            // price per landscape painting
+      final double CUSTOM_LANDSCAPE_PIC_PRICE = 50.0;          // price per custom landscape photo
+      final double CUSTOM_LANDSCAPE_PAINTING_PRICE = 75.0;     // price per custom landscape painting
+      // prices of brushes available for purchase
+      final double LARGE_ROUND_BRUSH_PRICE = 5.0;              // price per large round brush
+      final double MEDIUM_ROUND_BRUSH_PRICE = 3.0;             // price per medium round brush
+      final double SMALL_ROUND_BRUSH_PRICE = 2.0;              // price per small round brush
+      
+      
+      
+   }
+} 
